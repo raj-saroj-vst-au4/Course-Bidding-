eea80a7dce401d85c538d70d0bd942b41fc7fd14
raj-saroj-vst-au4 <raj.s.saroj@gmail.com>
2021-12-15 17:38:25 +0530

    fixing stats graph

--- public/js/portal.js
+++ public/js/portal.js
@@ -148,7 +148,77 @@ $(document).ready(function () {
       });
     }
   });
+
   $("#subject_stats").on("click", () => {
     window.location.href = "/stats-page";
   });
+
+  //Stats page
+  if (window.location.pathname == "/stats-page") {
+    renderGraph();
+  }
+  // $("#stats-link").on("click", () => {
+  //   window.location.href = "/stats-page";
+  // });
 });
+
+function renderGraph() {
+  var xAxisTerm1 = [];
+  var yAxisTerm1 = [];
+  var xAxisTerm2 = [];
+  var yAxisTerm2 = [];
+  $.ajax({
+    method: "POST",
+    url: "/api/fetch-stats",
+    statusCode: {
+      200: function (result) {
+        console.log(result);
+        for (var i = 0; i < result.length; i++) {
+          if (
+            result[i].course_term == "1" &&
+            result[i].course_students.length
+          ) {
+            console.log(
+              result[i].course_code +
+                " is a term 1 course with highest bid " +
+                result[i].course_students[0].student_bid
+            );
+            xAxisTerm1.push(result[i].course_code);
+            yAxisTerm1.push(result[i].course_students[0].student_bid);
+          } else if (
+            result[i].course_term == "2" &&
+            result[i].course_students.length
+          ) {
+            console.log(
+              result[i].course_code +
+                " is a term 2 course with highest bid " +
+                result[i].course_students[0].student_bid
+            );
+            xAxisTerm2.push(result[i].course_code);
+            yAxisTerm2.push(result[i].course_students[0].student_bid);
+          }
+        }
+        console.log(xAxisTerm1, yAxisTerm1);
+        new Chart("myChart", {
+          type: "bar",
+          data: {
+            labels: xAxisTerm1,
+            datasets: [
+              {
+                //   backgroundColor: barColors,
+                data: yAxisTerm1,
+              },
+            ],
+          },
+          options: {
+            legend: { display: false },
+            title: {
+              display: true,
+              text: "Most in-demand Subjects for Term 1",
+            },
+          },
+        });
+      },
+    },
+  });
+}
